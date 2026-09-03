eca756c5599a04154bdec76ca99cbbb74a511995
Divine-AC <divineabbachimdirim1@gmail.com>
2026-09-03 10:58:34 +0100

    my second push

---
+++ inventry system/config/databaseconfig.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const conn = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    }
+    catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1);};}
